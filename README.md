5f148c4f41179387df52e718f85dea6506174b9f
nverba <distantstate@gmail.com>
2015-04-28 19:17:18 +0100

    updated posts css & new filters for post mata data

--- README.md
+++ README.md
@@ -7,11 +7,3 @@ __Production checks:__
 
 __ToDo:__
 
-  - generate absolute permalinks
-
-  - Create index builder with image base64 conversion & embedding with above the fold css.
-  - Add full path to permalink
-
-  - use frontmatter in client to create page header
-
-  - add style to search results
--- src/js/filters.js
+++ src/js/filters.js
@@ -1,5 +1,10 @@
-angular.module('statique:filters', []).filter('parsekey', function(){
-  return function (text) {
-    return text.replace(/_/g, '-').toLowerCase();
-  };
-});
+angular.module('statique:filters', [])
+  .filter('parseKey', function(){
+    return function (text) {
+      return text.replace(/_/g, '-').toLowerCase();
+    };
+  }).filter('parseUrlDate', function(){
+    return function (text) {
+      return new Date(text.slice(0, 10));
+    };
+  });
