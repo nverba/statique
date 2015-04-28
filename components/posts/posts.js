a0f3ecd14ee46c96d01c9218d315ed67364e09be
nverba <distantstate@gmail.com>
2015-04-28 21:41:04 +0100

    made ignote posts root only

---
+++ components/posts/posts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('posts.component', ['ngNewRouter', 'statique:filters'])
+  .controller('PostsController', ['$http', '$location', PostsControllerFn]);
+
+function PostsControllerFn($http, $location) {
+
+  function fetchPosts(id) {
+    return $http.get('build/indexes/posts/' + id + '.json');
+  }
+
+  var allocatePosts = function (result) {
+    this.page = result.data;
+  }.bind(this);
+
+  fetchPosts($location.search().page || 0).then(allocatePosts);
+ 
+}
