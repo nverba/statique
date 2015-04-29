113d1821f54f6cbe538180a5f3082c2765bd1ca8
nverba <distantstate@gmail.com>
2015-04-29 19:27:37 +0100

    added valid value check in filters

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,10 +1,12 @@
 angular.module('statique:filters', [])
   .filter('parseKey', function(){
     return function (text) {
+      if (!text) { return; }
       return text.replace(/_/g, '-').toLowerCase();
     };
   }).filter('parseUrlDate', function(){
     return function (text) {
+      if (!text) { return; }
       return new Date(text.slice(0, 10));
     };
   });
