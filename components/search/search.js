803ac8bcd86b150ccd68c4faaa5c2c7ffd7ed9bd
nverba <distantstate@gmail.com>
2015-05-20 12:31:26 +0100

    added ordering by active tag count to search results - removed dupes

--- components/search/search.js
+++ components/search/search.js
@@ -13,6 +13,14 @@ function SearchControllerFn($rootScope, $location, $http, $q) {
     return string.toLowerCase();
   });
 
+  // count in & weed out the duplicates for ordering
+  var countUniq = Ra.uniqWith(function(a, b) {
+    if (a.title === b.title) {
+      b.count = b.count ? b.count + 1 : 2;
+      return true;
+    }
+  });
+
   var params = $location.search()["tags[]"];
   // coerce to array, params can be single string or array of strings
   var params_array  = params ? [].concat(params) : [];
@@ -29,7 +37,7 @@ function SearchControllerFn($rootScope, $location, $http, $q) {
   });
 
   var allocateResults = angular.bind(this, function () {
-    this.results = Ra.flatten(Ra.values(activeTagsOf(tag_cache)));
+    this.results = countUniq(Ra.flatten(Ra.values(activeTagsOf(tag_cache))));
   });
  
   // When all tags loaded
